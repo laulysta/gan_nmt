a5e5332a7eea94d998495a7be3a461af49f8984e
fducau <fnd212@nyu.edu>
2017-03-10 18:30:29 -0500

    Removing adversarial from amster

--- nmt/nmt.py
+++ nmt/nmt.py
@@ -102,83 +102,6 @@ def init_params(options):
 
     return params
 
-def build_encoder_adversarial(tparams, options):
-    trng = RandomStreams(1234)
-
-    # description string: #hidden_states x #samples
-    h_orig = tensor.matrix('h_orig', dtype='float32')
-    h_fake = tensor.matrix('h_fake', dtype='float32')
-    h_orig_mask = tensor.matrix('h_orig_mask', dtype='float32')
-    h_fake_mask = tensor.matrix('h_fake_mask', dtype='float32')
-
-    h_orig_r = h_orig[::-1]
-    h_fake_r = h_fake[::-1]
-    h_orig_r_mask = h_orig_mask[::-1]
-    h_fake_r_mask = h_fake_mask[::-1]
-
-    n_timesteps_orig = h_orig.shape[0]
-    n_timesteps_fake = h_fake.shape[0]
-
-    encoder = get_layer(options['encoder_adversarial'])[1]
-    proj_orig = encoder(tparams, h_orig, options, prefix='encoder_adersarial', mask=x_mask_orig)
-    proj_fake = encoder(tparams, h_fake, options, prefix='encoder_adersarial', mask=x_mask_fake)
-
-    proj_orig_r = encoder(tparams, h_orig_r, options, prefix='encoder_adersarial_r', mask=x_mask_orig_r)
-    proj_fake_r = encoder(tparams, h_fake_r, options, prefix='encoder_adersarial_r', mask=x_mask_fake_r)
-
-    B_orig = concatenate([proj_orig[0][-1], proj_orig_r[0][-1]], axis=proj_orig[0].ndim - 1)
-    B_fake = concatenate([proj_fake[0][-1], proj_fake_r[0][-1]], axis=proj_fake[0].ndim - 1)
-
-    inps = [h_orig, h_fake, h_orig_mask, h_fake_mask]
-    outs = [B_orig, B_fake]
-
-    encoder_adversarial = theano.function(inps, outs, name='encoder_adversarial', profile=profile)
-    return encoder_adversarial
-
-def build_discriminator_adversarial(tparams, options):
-    B_orig = tensor.matrix('B_orig', dtype='float32')
-    B_fake = tensor.matrix('B_fake', dtype='float32')
-
-    ff1 = get_layer(options['ff1_adversarial'])[1]
-    ff2 = get_layer(options['ff2_adversarial'])[1]
-    ff_out = get_layer(options['ff_out_adversarial'])[1]
-
-    D_orig = ff1(tparams, B_orig, options, prefix='ff_state', activ='relu')
-    D_orig = ff2(tparams, D_orig, options, prefix='ff_state', activ='relu')
-    D_orig = ff_out(tparams, D_orig, options, prefix='ff_state', activ='sigmoid')
-
-    D_fake = ff1(tparams, B_orig, options, prefix='ff_state', activ='relu')
-    D_fake = ff2(tparams, D_fake, options, prefix='ff_state', activ='relu')
-    D_fake = ff_out(tparams, D_fake, options, prefix='ff_state', activ='sigmoid')
-
-    inps = [B_orig, B_fake]
-    outs = [D_orig, D_fake]
-
-    discriminator_adversarial = theano.function(inps, outs, name='discriminator_adversarial', profile=profile)
-
-    return discriminator_adversarial
-
-
-def build_adversarial_discriminator_cost(tparams, options):
-    D_orig = tensor.matrix('D_orig', dtype='float32')
-    D_fake = tensor.matrix('D_fake', dtype='float32')
-
-    # Review
-    cost = -tensor.mean(D_orig * tensor.log(D_orig) + (1 - D_fake) * tensor.log(1 - D_fake))
-    inps = [D_orig, D_fake]
-    outs = [cost]
-
-    discriminator_adversarial_cost = theano.function(inps, outs, name='discriminator_adversarial_cost', profile=profile)
-    return discriminator_adversarial_cost
-
-
-def build_adversarial_generator_cost(encoder_adversarial, tparams, options):
-    D_fake = tensor.matrix('D_fake', dtype='float32')
-    cost = -tensor.mean((D_fake) * tensor.log(1 - D_fake))
-
-    adversarial_generator_cost = theano.function([D_fake], [cost], name='adversarial_generator_cost', profile=profile)
-    return adversarial_generator_cost
-
 
 def build_model(tparams, options):
     opt_ret = dict()
@@ -639,8 +562,6 @@ def train(dim_word=100,  # word vector dimensionality
     print 'Building optimizers...',
     # f_grad_shared, f_update = eval(optimizer)(lr, tparams, grads, inps, cost)
     f_update = eval(optimizer)(lr, tparams, grads, inps, cost)
-    #BUILD ADVERSARIAL OPTIMIZER
-    f_update_adversarial = eval(optimizer)(lr, tparams, grads_adversarial, cost_adversarial)
     print 'Done'
 
     print 'Optimization'
