01abae604749dd78529ffe836e115b242f360254
fducau <fnd212@nyu.edu>
2017-04-30 16:58:14 -0400

    adding adversarial_cost in train

--- nmt/nmt.py
+++ nmt/nmt.py
@@ -665,7 +665,10 @@ def train(dim_word=100,  # word vector dimensionality
           correlation_coeff=0.1,
           clip_c=0., 
           adversarial_mode='simple',
+          adversarial_cost='default',
           lambda_adv=1.):
+
+
     bad_counter = 0
     model_options = copy.copy(inspect.currentframe().f_locals)
     model_options['decoder_FR'] = 'gru_cond_FR'
