267bed1d6bf61523b0863b2a08c20eb95e18bc16
fducau <fnd212@nyu.edu>
2017-05-10 18:33:15 -0400

    Path change

--- nmt/translate_folder.sh
+++ nmt/translate_folder.sh
@@ -4,5 +4,5 @@
 for MODEL in $(ls $1/*.npz)
     do
         #wc $MODEL
-        python translate.py $MODEL ../data/data_vocab_europarl_en_de_h5/vocab.de.pkl ../data/data_vocab_europarl_en_de_h5/vocab.en.pkl ../data/en_de_txt/val_ende-ref.de.toc.low $MODEL.txt
+        python translate.py $MODEL ../data/data_vocab_europarl_en_de_h5/vocab.de.pkl ../data/data_vocab_europarl_en_de_h5/vocab.en.pkl ../data/en_de_txt/plus50_3k_europarl-v7.de-en.de.toc.low $MODEL.txt
     done
