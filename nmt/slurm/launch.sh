4fbb8b76d72ca3c1479ad10a5b8aa527c97dd1af
fducau <fnd212@nyu.edu>
2017-06-21 18:01:54 -0400

    Adding tables

--- nmt/slurm/launch.sh
+++ nmt/slurm/launch.sh
@@ -15,7 +15,8 @@ module load nltk/3.2.2
 module load scikit-learn/intel/0.18.1
 module load cuda/8.0.44
 module load cudnn/8.0v5.1
-
+module load pytables/intel/2.4.0
+module load pytables/intel/3.4.2
 
 
 SRC=/home/fnd212/repos/nmt/nmt
@@ -23,3 +24,5 @@ DST=/home/fnd212/repos/nmt/nmt/saved_models/de-en/baseline_dropout0-5/
 
 cd $SRC ; THENAO_FLAGS=floatX=float32,device=gpu \
 		  python -u nmt.py > $DST/log.log
+
+#1156724
